b8bb156ebe15bc45b5f102374c1e5bdd65cfe10e
jiro.kirameki <ec2-user@ip-172-31-29-130.us-east-2.compute.internal>
2018-07-05 07:23:40 +0000

    haikei

--- onoshin/resources/views/layouts/app.blade.php
+++ onoshin/resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Onoshin</title>
-    <link rel="shortcut icon" href="nameko(2).ico" />
+    <link rel="shortcut icon" href="nameko (2).ico" />
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
