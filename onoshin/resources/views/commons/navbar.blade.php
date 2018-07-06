d8e31af31bd332fbde60ddd34144971f6f70d086
jiro.kirameki <ec2-user@ip-172-31-34-226.us-east-2.compute.internal>
2018-07-06 01:30:08 +0000

    navbarlogo

--- onoshin/resources/views/commons/navbar.blade.php
+++ onoshin/resources/views/commons/navbar.blade.php
@@ -8,7 +8,8 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="onoshin"></a>
+                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/SmartGoalslogo.png") }}" alt="onoshin"></a>
+
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
