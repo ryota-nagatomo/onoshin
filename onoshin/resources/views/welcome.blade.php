42decfcb9be2e8354f0e5e3b35a55d2b27d6885a
jiro.kirameki <ec2-user@ip-172-31-29-130.us-east-2.compute.internal>
2018-07-09 06:23:01 +0000

    toppage

--- onoshin/resources/views/welcome.blade.php
+++ onoshin/resources/views/welcome.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.app')
 
 @section('cover')
+
+            
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
                 <h1>Welcome to SmartGoals</h1>
-                @if (!Auth::check())
-                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">SmartGoalsを始める</a>
-                @endif
-               
+                <div class="button">
+                    @if (!Auth::check())
+                        <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg" style="font-size:20pt">SmartGoalsを始める</a>
+                    @endif
+                </div>       
             </div>
         </div>
     </div>
+        
+  
 @endsection
 
 @section('content')
