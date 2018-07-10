00fb4128ff903d9f87e74f23dbf7017ec93447bf
ryota.nagatomo <ec2-user@ip-172-31-46-39.us-east-2.compute.internal>
2018-07-10 05:40:09 +0000

    register form

--- onoshin/resources/views/auth/register.blade.php
+++ onoshin/resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
 <div class="row">
     <div class="col-xs-offset-3 col-xs-6">
         <div class="panel panel-default">
-            <div class="panel-heading">Please fill in the following necessary infomations</div>
+            <div class="panel-heading" style="font-size:large">Please fill in the following necessary infomations.</div>
             <div class="panel-body">
                 {!! Form::open(['route' => 'signup.post']) !!}
                     <div class="form-group">
