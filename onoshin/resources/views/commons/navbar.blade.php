472c564f606f74982244dec7751576cc560262a0
jiro.kirameki <ec2-user@ip-172-31-29-130.us-east-2.compute.internal>
2018-07-11 07:28:21 +0000

    toppageimage

--- onoshin/resources/views/commons/navbar.blade.php
+++ onoshin/resources/views/commons/navbar.blade.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.jpg") }}" alt="onoshin"></a>
+                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo2.jpg") }}" alt="onoshin"></a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
