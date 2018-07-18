66d25a9fd87be976ae18e5d2c49dd1d9e1e239b1
hiro.tsutsumi <ec2-user@ip-172-31-18-142.us-east-2.compute.internal>
2018-07-18 05:27:38 +0000

    keep and copy

--- onoshin/resources/views/ranking/index.blade.php
+++ onoshin/resources/views/ranking/index.blade.php
@@ -3,15 +3,38 @@
 	    <thead>
 	        <tr>
 	        	<th>Smart Goals</th>
+	        	<th>Keep</th>
+	        	<th></th>
             </tr>
        </thead>
        <tbody>
            <?php foreach($goals as $goal): ?>
             <tr>
-                <td>{{ $goal->content }}</td>
+                <td id=targetID>{{ $goal->content }}</td>
                 <td>@include('good_user.good_button')</td>
+                <td><button id='btnCopy' class="btn"><span class='glyphicon glyphicon-copy'> copy</span></button></td>
             </tr>
             <?php endforeach; ?>
         </tbody>
    </table>
 </div>
+
+<script>
+    document.querySelector('#btnCopy').addEventListener("click", function(){
+      var element = document.querySelector('#targetID');
+      var selection = window.getSelection();
+      var range = document.createRange();
+      range.selectNodeContents(element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      //console.log('選択された文字列: ', selection.toString());
+      var succeeded = document.execCommand('copy');
+      if (succeeded) {
+          alert('コピーが成功しました！');
+      } else {
+          alert('コピーが失敗しました!');
+      }
+      selection.removeAllRanges();
+    
+    });
+</script>
